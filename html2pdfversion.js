2a9d97252896fb12e49bac883442cb8856fc312e
Younes <106814633+younes-boudjemaa@users.noreply.github.com>
2024-09-05 12:22:31 +0200

    Add files via upload

--- html2pdfversion.js
+++ html2pdfversion.js
@@ -5,6 +5,10 @@ function genererPDF() {
   const paragraphes = document.querySelectorAll("p");
   paragraphes.forEach((p) => (p.style.display = "block"));
 
+  // Ne pas Afficher les button dans le pdf
+  const button = document.querySelectorAll("button");
+  button.forEach((button) => (button.style.display = "none"));
+
   // Options pour le PDF (personnalisation de la mise en page)
   const options = {
     margin: 0.5, // Marges du PDF
@@ -22,5 +26,14 @@ function genererPDF() {
     .then(() => {
       // Réinitialiser le formulaire après génération du PDF
       document.getElementById("formulaire").reset();
+
+      // Rafraîchir la page après génération du PDF
+      window.location.reload(); // Rafraîchir la page
     });
+
+  //   // Fonction pour envoyer par email
+  //   function envoyerEmail() {
+  //     const mailtoLink = `mailto:recipient@example.com?subject=Dossier%20EHPAD&body=Veuillez%20trouver%20ci-joint%20le%20dossier%20EHPAD.%0A%0AN'oubliez%20pas%20de%20joindre%20le%20fichier%20PDF%20généré.`;
+  //     window.location.href = mailtoLink;
+  //   }
 }
